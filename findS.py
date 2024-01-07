1fd32642a6c84a1bba4fc698c22a99626c83f8f9
vasista10 <shashankhnp90360@gmail.com>
2024-01-07 22:24:49 +0530

    added aostar

---
+++ findS.py
@@ -0,0 +1,18 @@
+import csv
+file = open(r"C:\Users\shash\OneDrive\Documents\github\Food\csvfiles\p2.csv") # Read CSV data
+data = list(csv.reader(file)) # Convert into list format
+length = len(data[0]) - 1 # -1 because we don't need the target variable
+h = ['0'] * length # Initial hypothesis
+print("Initial Hypothesis:", h)
+print('Data:')
+for i in data:
+    print(i)
+col = data.pop(0) # Removing the column names
+for i in range(len(data)):
+    if data[i][length] == 'yes': # Considering only the positive examples
+        for j in range(len(data[i]) - 1): # Not considering the target variable
+            if h[j] == '0':
+                h[j] = data[i][j] # If 0 then copy the data
+            if h[j] != data[i][j]: # If not equal to the previous hypothesis,then put '?'
+                h[j] = '?'
+print("Final Hypothesis (Most Specific):", h)
